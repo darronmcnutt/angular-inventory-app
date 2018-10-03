0b9c733a7d04af8614aa174db93b5e69be737875
Darron McNutt <darronlmcnutt@gmail.com>
2018-10-02 19:21:33 -0500

    final version

---
+++ src/app/price-display/price-display.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-price-display',
+  template: `
+  <div class="price-display">\${{ price }}</div>
+  `
+})
+export class PriceDisplayComponent {
+
+  @Input() price: number;
+
+}
